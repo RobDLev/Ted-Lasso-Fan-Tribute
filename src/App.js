2d7ff31295924dd4ae00d08526144894193a6cd3
RobDLev <75288391+RobDLev@users.noreply.github.com>
2021-06-06 12:08:50 -0700

    Added logo, navbar

--- src/App.js
+++ src/App.js
@@ -4,8 +4,27 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <img alt="Blue Skies" src="blueSkies.jpeg" />
+        <img id="blue-skies" alt="Blue Skies" src="blueSkies.jpeg" />
       </div>
+      <header className="clearfix">
+        <img
+          id="greyhounds-logo"
+          alt="Greyhounds Logo"
+          src="AFC Richmond Crest.png"
+        />
+      </header>
+      <nav id="main-menu">
+        <ul>
+          <li>Covid-19</li>
+          <li>Tickets</li>
+          <li>Schedule</li>
+          <li>News</li>
+          <li>Videos</li>
+          <li>Gameday</li>
+          <li>Stadium</li>
+          <li>Shop</li>
+        </ul>
+      </nav>
     </div>
   );
 }
