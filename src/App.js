13a5c4c438ffc1a17eece093855feb4ca9c79d91
RobDLev <75288391+RobDLev@users.noreply.github.com>
2021-06-15 20:50:10 -0700

    added images, fixed bootstrap documentation

--- src/App.js
+++ src/App.js
@@ -5,73 +5,123 @@ export default function App() {
     <div className="App">
       <div className="container">
         <img id="blue-skies" alt="Blue Skies" src="blueSkies.jpeg" />
-      </div>
-      <header className="clearfix">
-        <img
-          id="greyhounds-logo"
-          alt="Greyhounds Logo"
-          src="AFC Richmond Crest.png"
-        />
-        <div className="social_sharing">
-          <a
-            href="https://www.facebook.com/TedLassoSeason2/"
-            target="_blank"
-            rel="nofollow noreferrer"
-          >
-            <img
-              id="social-link"
-              src="Facebook_logo_(square).png"
-              alt="facebook link"
-            />
-          </a>
-          <a
-            href="https://twitter.com/afcrichmond?lang=en"
-            target="_blank"
-            rel="nofollow noreferrer"
-          >
-            <img
-              id="social-link"
-              src="Twitter social icons - square - blue.png"
-              alt="twitter link"
-            />
-          </a>
-          <a
-            href="https://www.instagram.com/afc_richmond/?hl=en"
-            target="_blank"
-            rel="nofollow noreferrer"
-          >
-            <img
-              id="social-link"
-              src="Instagram_Glyph_Gradient_RGB.png"
-              alt="instagram link"
-            />
-          </a>
-          <a
-            href="https://www.youtube.com/c/AppleTV/featured"
-            target="_blank"
-            rel="nofollow noreferrer"
-          >
-            <img
-              id="social-link"
-              src="youtube_social_square_red.png"
-              alt="youtube link"
-            />
-          </a>
+        <header className="clearfix">
+          <img
+            id="greyhounds-logo"
+            alt="Greyhounds Logo"
+            src="AFC Richmond Crest.png"
+          />
+          <div className="social_sharing">
+            <a
+              href="https://www.facebook.com/TedLassoSeason2/"
+              target="_blank"
+              rel="nofollow noreferrer"
+            >
+              <img
+                id="social-link"
+                src="Facebook_logo_(square).png"
+                alt="facebook link"
+              />
+            </a>
+            <a
+              href="https://twitter.com/afcrichmond?lang=en"
+              target="_blank"
+              rel="nofollow noreferrer"
+            >
+              <img
+                id="social-link"
+                src="Twitter social icons - square - blue.png"
+                alt="twitter link"
+              />
+            </a>
+            <a
+              href="https://www.instagram.com/afc_richmond/?hl=en"
+              target="_blank"
+              rel="nofollow noreferrer"
+            >
+              <img
+                id="social-link"
+                src="Instagram_Glyph_Gradient_RGB.png"
+                alt="instagram link"
+              />
+            </a>
+            <a
+              href="https://www.youtube.com/c/AppleTV/featured"
+              target="_blank"
+              rel="nofollow noreferrer"
+            >
+              <img
+                id="social-link"
+                src="youtube_social_square_red.png"
+                alt="youtube link"
+              />
+            </a>
+          </div>
+        </header>
+        <nav id="nav-bar">
+          <ul>
+            <li>Covid-19</li>
+            <li>Tickets</li>
+            <li>Schedule</li>
+            <li>News</li>
+            <li>Videos</li>
+            <li>Gameday</li>
+            <li>Stadium</li>
+            <li>Shop</li>
+          </ul>
+          <i id="search-icon" className="fas fa-search"></i>
+        </nav>
+        <div>
+          <main>
+            <button type="button" class="btn btn-primary">
+              Primary
+            </button>
+            <div className="row row-cols-3">
+              <div className="col-sm-4">
+                <div className="card">
+                  <img
+                    src="goal celebration.jpeg"
+                    className="card-img-top"
+                    alt="Rojas and team celebrate his goal"
+                  />
+                  <div class="card-body">
+                    <p class="card-text">
+                      BUY NOW: AFC Richmond vs. Birmingham City tickets for June
+                      19
+                    </p>
+                  </div>
+                </div>
+                <div className="card">
+                  <img
+                    src="goal celebration.jpeg"
+                    className="card-img-top"
+                    alt="Rojas and team celebrate his goal"
+                  />
+                  <div class="card-body">
+                    <p class="card-text">
+                      BUY NOW: AFC Richmond vs. Birmingham City tickets for June
+                      19
+                    </p>
+                  </div>
+                </div>
+                <div className="card">
+                  <img
+                    src="goal celebration.jpeg"
+                    className="card-img-top"
+                    alt="Rojas and team celebrate his goal"
+                  />
+                  <div class="card-body">
+                    <p class="card-text">
+                      BUY NOW: AFC Richmond vs. Birmingham City tickets for June
+                      19
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </main>
         </div>
-      </header>
-      <nav id="nav-bar">
-        <ul>
-          <li>Covid-19</li>
-          <li>Tickets</li>
-          <li>Schedule</li>
-          <li>News</li>
-          <li>Videos</li>
-          <li>Gameday</li>
-          <li>Stadium</li>
-          <li>Shop</li>
-        </ul>
-        <i id="search-icon" class="fas fa-search"></i>
-      </nav>
+      </div>
     </div>
   );
 }
